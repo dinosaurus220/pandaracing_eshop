32ad9169a2616515ff7adce02d0fa1105845e8a2
Vojtěch Vlachovský <92089130+Kropensaft@users.noreply.github.com>
2023-06-02 23:11:54 +0200

    kosik

---
+++ src/components/Cart.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function Cart() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems((prevCartItems) => [...prevCartItems, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems.filter((item) => item.id !== productId)
+    );
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price, 0);
+  };
+
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              {item.name} - ${item.price}
+              <button onClick={() => removeFromCart(item.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <p>Total: ${calculateTotal()}</p>
+      <button onClick={clearCart}>Clear Cart</button>
+      {/* Render additional cart-related components, such as checkout button */}
+    </div>
+  );
+}
+
+export default Cart;
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import Footer from './Footer';
 import Account from './Account';
+import Cart from './Cart';
 import Engine from './categories/engineSys';
 import Chassis from './categories/chassisSys';
 import Brakes from './categories/brakesSys';
@@ -17,5 +18,6 @@ export{
     Engine,
     Chassis,
     Brakes,
-    Spacers
+    Spacers,
+    Cart
 }
